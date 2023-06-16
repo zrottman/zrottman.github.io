3c64d65f673a2af1345b6aece0d6e29aa226109b
zrottman <zrottman@example.com>
2023-06-15 20:23:10 -0700

    Commit new blog and resources

---
+++ assets/code/pairs.scm
@@ -0,0 +1,48 @@
+(define (make-rat n d) (cons n d))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  (newline))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x)
+                  (denom y))
+               (* (numer y)
+                  (denom x)))
+            (* (denom x)
+               (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x)
+                  (denom y))
+               (* (numer y)
+                  (denom x)))
+            (* (denom x)
+               (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x)
+               (numer y))
+            (* (denom x)
+               (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x)
+               (denom y))
+            (* (denom x)
+               (numer y))))
+
+(define (cons a b)
+  (lambda (pick)
+    (cond ((= pick 1) a)
+          ((= pick 2) b))))
+
+(define (car x) (x 1))
+(define (cdr x) (x 2))
